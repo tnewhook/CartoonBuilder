b39a5a667bb9ab6917f0f66bdc86f23d05f384ea
tnewhook <tnewhook@gmail.com>
2020-11-11 09:24:42 -0500

    image event handler in progress

--- cartoonBuilder.js
+++ cartoonBuilder.js
@@ -6,20 +6,20 @@ var toolbar = {
     text: {
         name: "text",
         action: "modifyCanvas",
-        altText: "add some text",
+        altText: "add some text"
 
     },
     image: {
         name: "image",
         action: "modifyCanvas",
-        altText: "add a background image",
+        altText: "add a background image"
     },
     thought: {
         altText: "add a thought bubble",
         tl: {
             name: "tlThought",
             action: "modifyCanvas",
-            altText: "add an thought bubble with the small bubbles toward the top left",
+            altText: "add an thought bubble with the small bubbles toward the top left"
         },
         tr: {
             name: "trThought",
@@ -69,22 +69,22 @@ var toolbar = {
     add: {
         name: "add",
         action: "modifyCanvas",
-        altText: "add a new comic panel",
+        altText: "add a new comic panel"
     },
     sub: {
         name: "sub",
         action: "modifyCanvas",
-        altText: "remove selected comic panel",
+        altText: "remove selected comic panel"
     }
 };
 
 
 function buildToolbarList(toolbar) {
     /*get items from toolbar object, create wrapper, add images, add to wrapper
-		add button actions to all buttons
-		creates list,adds items to listText
-		creates sublist items, add to sublist, add to list
-		*/
+    add button actions to all buttons
+    creates list,adds items to listText
+    creates sublist items, add to sublist, add to list
+    */
 
     var toolbarDiv, toolbarList, element, listItem, listText, listImg;
     toolbarDiv = this.document.createElement('div');
@@ -162,6 +162,10 @@ class Canvas {
         let canvasInstance = this;
     }
 
+    preventDefault(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
     changeCurrentCanvas(toolbar, currentCanvas) {
         var activeCanvas = document.getElementsByClassName("activeCanvas");
         while (activeCanvas.length) {
@@ -171,23 +175,35 @@ class Canvas {
         document.getElementById(currentCanvasWrapper).className += " 	activeCanvas";
     }
 
-    createCanvas = function (CartoonWrapper) {
+    createCanvas(CartoonWrapper) {
+        let fabricCanvas, canvasWrapper, canvasElement, currentCanvasInstance, currentCanvasID, canvasContext;
         ++canvasCounter;
         currentCanvas = canvasCounter;
-        let canvasWrapper = document.createElement('div');
+        canvasWrapper = document.createElement('div');
         canvasWrapper.id = "canvasWrapper" + currentCanvas;
         canvasWrapper.className = "canvasWrapperClass";
-        let canvasElement = document.createElement('canvas');
+        canvasElement = document.createElement('canvas');
         canvasElement.id = "canvasElement" + currentCanvas;
         canvasElement.className = "canvasClass";
         canvasWrapper.appendChild(canvasElement);
+        fabricCanvas = new fabric.Canvas(canvasElement);
         document.getElementById("CartoonWrapper").appendChild(canvasWrapper);
-        let currentCanvasInstance = this;
+        currentCanvasInstance = this;
         canvasElement.addEventListener("click", function (e) {
-            let currentCanvasID = e.target.id;
+            currentCanvasID = e.target.id;
             currentCanvas = currentCanvasID.replace('canvasElement', '');
             currentCanvasInstance.changeCurrentCanvas(toolbar, currentCanvas);
         });
+        canvasContext = this;
+        canvasWrapper.addEventListener('dragenter', canvasContext.preventDefault, false);
+        canvasWrapper.addEventListener('dragleave', canvasContext.preventDefault, false);
+        canvasWrapper.addEventListener('dragover', canvasContext.preventDefault, false);
+        canvasWrapper.addEventListener('drop', canvasContext.preventDefault, false);
+        canvasWrapper.addEventListener('drop', function () {
+            canvasContext.handleDrop(this.event), false;
+
+        });
+
         this.changeCurrentCanvas(toolbar, currentCanvas);
     }
 
@@ -202,21 +218,158 @@ class Canvas {
     addBubble(bubbleType) {
         console.log(bubbleType);
     };
-
-    addBackImg(textString) {
-        console.log(textString);
-    };
-
     addText(textString) {
         console.log(textString);
     };
+    /*///////////////////////////Functions for handling changing background images
+    clicking the 'add image' button pulls up a file selector, which
+    	*/
+
+    handleFiles(files) {
+        for (var i = 0, len = files.length; i < len; i++) {
+            if (this.validateImage(files[i]))
+                console.log('fileInfo', files[i]);
+            this.previewAnduploadImage(files[i]);
+        }
+    };
+
+    addBackImg(textString) {
+        let file, fakeInput, addBackImg;
+        addBackImg = this;
+        console.log(textString);
+        fakeInput = document.createElement("input");
+        fakeInput.type = "file";
+        fakeInput.accept = "image/*";
+        fakeInput.multiple = true;
+        fakeInput.click();
+        fakeInput.addEventListener("change", function () {
+            file = fakeInput.files;
+            addBackImg.handleFiles(file);
+        });
+    };
+
+    handleDrop(e) {
+        let dt;
+        console.log('we have a drop');
+        dt = e.dataTransfer,
+            console.log('dt', dt);
+        files = dt.files;
+        if (files.length) {
+            this.handleFiles(files);
+        } else {
+            // check for img
+            var html = dt.getData('text/html'),
+                match = html && /\bsrc="?([^"\s]+)"?\s*/.exec(html),
+                url = match && match[1];
+            if (url) {
+                this.uploadImageFromURL(url);
+                return;
+            }
+        }
+    }
+
+    uploadImageFromURL(url) {
+        var img = new Image;
+        //var c = cartoonStrip[currentCanvas-1].fabricCanvas;
+        var c = cartoonStrip[currentCanvas - 1].canvasElement;
+        var ctx = c.getContext("2d");
+
+        img.onload = function () {
+            c.width = this.naturalWidth; // update canvas size to match image
+            c.height = this.naturalHeight;
+            ctx.drawImage(this, 0, 0); // draw in image
+            c.toBlob(function (blob) { // get content as PNG blob
+
+                // call our main function
+                handleFiles([blob]);
+
+            }, "image/png");
+        };
+        img.onerror = function () {
+            alert("Error in uploading");
+        }
+        img.crossOrigin = ""; // if from different origin
+        img.src = url;
+    }
+
+    validateImage(image) {
+        // check the type
+        var validTypes = ['image/jpeg', 'image/png', 'image/gif'];
+        if (validTypes.indexOf(image.type) === -1) {
+            alert("Invalid File Type");
+            return false;
+        }
+
+        // check the size
+        var maxSizeInBytes = 10e6; // 10MB
+        if (image.size > maxSizeInBytes) {
+            alert("File too large");
+            return false;
+        }
+        return true;
+    }
+
+    previewAnduploadImage(image) {
+        //TODO: FINISH ADDING UPLOADED/DRAGGED IMAGE TO DIV
+        // container
+        var imgView = document.createElement("div");
+        imgView.className = "image-view";
+        cartoonStrip[currentCanvas].appendChild(imgView);
+
+        // previewing image
+        var img = document.createElement("img");
+        imgView.appendChild(img);
+
+        // progress overlay
+        var overlay = document.createElement("div");
+        overlay.className = "overlay";
+        imgView.appendChild(overlay);
+
+        // read the image...
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            img.src = e.target.result;
+        }
+        reader.readAsDataURL(image);
+
+        // create FormData
+        var formData = new FormData();
+        formData.append('image', image);
+
+        // upload the image
+        var uploadLocation = 'UPLOAD_LOCATION';
+
+        var ajax = new XMLHttpRequest();
+        ajax.open("POST", uploadLocation, true);
+
+        ajax.onreadystatechange = function (e) {
+            if (ajax.readyState === 4) {
+                if (ajax.status === 200) {
+                    // done!
+                } else {
+                    // error!
+                }
+            }
+        }
+
+        ajax.upload.onprogress = function (e) {
+            // change progress
+            // (reduce the width of overlay)
+
+            var perc = (e.loaded / e.total * 100) || 100,
+                width = 100 - perc;
+
+            overlay.style.width = width;
+        }
+        ajax.send(formData);
+    }
 }
+
 
 function createCanvasStructure(cartoonWrapper) {
     let toolbarDiv = buildToolbarList(toolbar);
     cartoonWrapper.appendChild(toolbarDiv);
 }
-
 
 function initializePage() {
     cartoonWrapper = document.getElementById("CartoonWrapper");
@@ -254,6 +407,5 @@ function modifyCanvas(changeType) {
         cartoonStrip[currentCanvas - 1].addBubble(changeType);
         break;
     default:
-
     }
 }
